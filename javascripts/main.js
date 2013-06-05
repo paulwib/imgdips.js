90ca8016246d5629d39fd805e755c3a033c59f8d
Paul Willoughby <paul@fivetide.com>
2013-06-05 17:12:13 +0100

    Init imgdips on load

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,31 +1,52 @@
 (function() {
+
+	/**
+	 * Default settings
+	 */
 	var settings = {
 		devicePixelRatioNames: {
 			'@2x': 1.5
 		}
 	};
 
+	/**
+	 * Reset all images ot their original src attributes
+	 */
 	function reset(){
 		$('img.dips').each(function(i, img) {
 			$(img).attr('src', $(img).data('original-src'));
 		});
 	}
 
+	/**
+	 * Set the devicePixelRatio threshold for triggering
+	 * @2x images
+	 */
 	function setThreshold(threshold) {
 		reset();
 		settings.devicePixelRatioNames['@2x'] = threshold;
 		ImgDips.init(settings);
 	}
 
+	/**
+	 * Initlaise images and event handlers
+	 */
 	function init(){
+
+		// Store original img src so we can revert later
 		$('img.dips').each(function(i, img) {
 			$(img).data('original-src', $(img).attr('src'));
 		});
+
+		// Form handler
 		$('#set-dip-ratio-threshold').on('submit', function(e) {
 			e.preventDefault();
 			var threshold = $(':checked', this).val();
 			setThreshold(threshold);
 		});
+
+		// Start with defaults
+		ImgDips.init();
 	}
 
 	$(init);
